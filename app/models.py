a083f8869df140c47c05a9c910dbe9ae7baab9fa
Anurag Singh <anurag504singh@gmail.com>
2025-02-24 20:09:53 +0530

    updated model

--- app/models.py
+++ app/models.py
@@ -2,20 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Student(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    roll_no = models.CharField(max_length=15,unique=True)
+    username = models.OneToOneField(User,on_delete=models.CASCADE,unique=True)
+    password = models.CharField(max_length=15)
 
 class Teacher(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    username = models.OneToOneField(User,on_delete=models.CASCADE,unique=True)
+    password = models.CharField(max_length=15)
 
 class Subject(models.Model):
     name = models.CharField(max_length=20)
     code = models.CharField(max_length=20, unique=True)
     teacher = models.ForeignKey(Teacher,on_delete=models.CASCADE)
+    student = models.ManyToManyField(Student,related_name='subject')
 
 
 class Attendance(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE, related_name='attendances')
     subject = models.ForeignKey(Subject,on_delete=models.CASCADE, related_name='Subject_attendances')
     date = models.DateField()
-    is_present = models.BooleanField(default=False)git 
+    is_present = models.BooleanField(default=False)
